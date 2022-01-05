7b607593332a63944ad1ccebe37d575883b63988
Adebayor <idrisadeebayorr12@gmail.com>
2022-01-05 10:05:59 +0100

    first commit

---
+++ collection.js
@@ -0,0 +1,30 @@
+// db.contactlist.insert({
+//     Last_name:  'Ben Lahmer', 
+//     First_name: 'Fares', 
+//     Email: 'fares@gmail.com', 
+//     age: '26',
+// })
+// db.contactlist.insertMany([
+// {
+//     Last_name: 'Kefi',
+//     First_name: 'Seif',
+//     Email: 'kefi@gmail.com', 
+//     age: '15',
+// },
+// {
+//     Last_name: 'Fatnassi',
+//     First_name: 'Sarra', 
+//     Email: 'sarra.f@gmail.com', 
+//     age: '40'
+// },
+// {
+//     Last_name: 'Ben Yahia', 
+//     First_name: 'Rym', 
+//     age:'4'
+// },
+// {
+//     Last_name: 'Cherif', 
+//     First_name: 'Sami', 
+//     age:'3'
+// }
+// ])
